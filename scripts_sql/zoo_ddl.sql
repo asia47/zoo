fc758461fb9e026cc34f98f5578553a6407e3c00
asia47 <canoli628@gmail.com>
2020-02-26 18:58:12 +0100

    engadimos scripts ddl e dml

---
+++ scripts_sql/zoo_ddl.sql
@@ -0,0 +1,157 @@
+-- DDL zoo
+
+-- CREACION DE LA BD
+/*USE master
+GO
+
+DROP DATABASE IF EXISTS zoo
+GO
+
+CREATE DATABASE zoo
+GO*/
+
+USE zoo
+GO
+
+-- CREACION DE TABLAS
+-- borrado de tablas
+
+
+IF EXISTS (SELECT name FROM sysobjects WHERE name = 'animal_espectaculo_cuidador' AND type = 'U')
+	DROP TABLE animal_espectaculo_cuidador
+GO
+
+IF EXISTS (SELECT name FROM sysobjects WHERE name = 'animal_veterinario' AND type = 'U')
+	DROP TABLE animal_veterinario
+GO
+
+IF EXISTS (SELECT name FROM sysobjects WHERE name = 'animal_cuidador' AND type = 'U')
+	DROP TABLE animal_cuidador
+GO
+
+IF EXISTS (SELECT name FROM sysobjects WHERE name = 'recinto_limpiador' AND type = 'U')
+	DROP TABLE recinto_limpiador
+GO
+
+IF EXISTS (SELECT name FROM sysobjects WHERE name = 'recinto_mantenimiento' AND type = 'U')
+	DROP TABLE recinto_mantenimiento
+GO
+
+IF EXISTS (SELECT name FROM sysobjects WHERE name = 'espectaculo' AND type = 'U')
+	DROP TABLE espectaculo
+GO
+
+IF EXISTS (SELECT name FROM sysobjects WHERE name = 'animal' AND type = 'U')
+	DROP TABLE animal
+GO
+
+IF EXISTS (SELECT name FROM sysobjects WHERE name = 'empleado' AND type = 'U')
+	DROP TABLE empleado
+GO
+
+IF EXISTS (SELECT name FROM sysobjects WHERE name = 'recinto' AND type = 'U')
+	DROP TABLE recinto
+GO
+
+IF EXISTS (SELECT name FROM sysobjects WHERE name = 'tarifa' AND type = 'U')
+	DROP TABLE tarifa
+GO
+
+IF EXISTS (SELECT name FROM sysobjects WHERE name = 'turno' AND type = 'U')
+	DROP TABLE turno
+GO
+
+--  entidades primarias
+CREATE TABLE turno (
+	id integer PRIMARY KEY not null,
+	fecha_inicio datetime not null,
+	fecha_fin datetime not null,
+	hora_inicio varchar(8) not null default '09:00:00',
+	hora_fin varchar(8) not null default '17:00:00'
+)
+GO
+
+CREATE TABLE tarifa (
+	id integer PRIMARY KEY not null,
+	precio money not null default 0,
+	nombre varchar(20)
+)
+GO
+
+CREATE TABLE recinto (
+	id integer PRIMARY KEY not null,
+	nombre varchar(20),
+	localizacion varchar(5) not null,
+	dimension integer not null
+)
+GO
+
+CREATE TABLE empleado (
+	id integer PRIMARY KEY not null,
+	tipo varchar(20) not null,
+	nombre varchar(20) not null,
+	dni varchar(12) not null,
+	nss varchar(12) not null,
+	telefono integer not null,
+	direccion varchar(50) not null,
+	id_turno integer FOREIGN KEY REFERENCES turno(id) not null
+)
+GO
+
+CREATE TABLE animal (
+	id integer PRIMARY KEY not null,
+	nombre varchar(30) not null,
+	especie varchar(20),
+	sexo char(1),
+	color varchar(20),
+	fecha_nacimiento date,
+	nacimiento_en_cautiverio bit,
+	id_padre integer FOREIGN KEY REFERENCES animal(id),
+	id_madre integer FOREIGN KEY REFERENCES animal(id),
+	id_recinto integer FOREIGN KEY REFERENCES recinto(id)
+)
+GO
+
+CREATE TABLE espectaculo (
+	id integer PRIMARY KEY not null,
+	nome varchar(20) not null,
+	fecha datetime not null,
+	id_tarifa integer FOREIGN KEY REFERENCES tarifa(id) not null,
+	id_recinto integer FOREIGN KEY REFERENCES recinto(id) not null
+)
+GO
+
+--  relaciones
+CREATE TABLE recinto_mantenimiento (
+	id_mantenimiento integer FOREIGN KEY REFERENCES empleado(id) not null,
+	id_recinto integer FOREIGN KEY REFERENCES recinto(id) not null
+)
+GO
+
+CREATE TABLE recinto_limpiador (
+	id_limpiador integer FOREIGN KEY REFERENCES empleado(id) not null,
+	id_recinto integer FOREIGN KEY REFERENCES recinto(id) not null
+)
+GO
+
+CREATE TABLE animal_cuidador (
+	id_cuidador integer FOREIGN KEY REFERENCES empleado(id) not null,
+	id_animal integer FOREIGN KEY REFERENCES animal(id) not null
+)
+GO
+
+CREATE TABLE animal_veterinario (
+	id_veterinario integer FOREIGN KEY REFERENCES empleado(id) not null,
+	id_animal integer FOREIGN KEY REFERENCES animal(id) not null
+)
+GO
+
+CREATE TABLE animal_espectaculo_cuidador (
+	id_cuidador integer FOREIGN KEY REFERENCES empleado(id) not null,
+	id_espectaculo integer FOREIGN KEY REFERENCES espectaculo(id) not null,
+	id_animal integer FOREIGN KEY REFERENCES animal(id) not null,
+	horario varchar(50) not null
+)
+GO
+
+
---
+++ scripts_sql/zoo_dml.sql
@@ -0,0 +1,91 @@
+--zoo dml
+USE zoo
+GO
+
+INSERT INTO turno (id, fecha_inicio, fecha_fin, hora_inicio, hora_fin)
+	VALUES 
+		(1, '2020-01-01', '2020-12-31', '09:00:00', '17:00:00'),
+		(2, '2020-01-01', '2020-12-31', '17:00:00', '01:00:00'),
+		(3, '2020-01-01', '2020-12-31', '01:00:00', '09:00:00')
+GO
+
+INSERT INTO tarifa (id, precio, nombre)
+	VALUES
+		(1, 15, 'Básica'),
+		(2, 20, 'Normal'),
+		(3, 25, 'VIP')
+GO
+
+INSERT INTO recinto (id, nombre, localizacion, dimension)
+	VALUES 
+		(1, 'Paquistán', '4B', '100100'),
+		(2, 'Irán', '3C', '200300'),
+		(3, 'Irak', '2Z', '400200')
+GO
+
+INSERT INTO empleado (id, tipo, nombre, dni, nss, telefono, direccion, id_turno)
+	VALUES
+		(1, 'Cuidador', 'Manuel Charlín', '23785264N', '128723763', '986873789', 'O Freixo 12', 1),
+		(2, 'Veterinario', 'Paco Vázquez', '23785265O', '128723764', '986873780', 'O Medrón 23', 2),
+		(3, 'Limpiador', 'Álvaro Cotino', '23785266P', '128723765', '986873781', 'Parameán 10', 3),
+		(4, 'Mantenimiento', 'Álvaro Lapuerta', '23489877Q', '123678456', '986673982', 'O Testal', 2)
+GO
+
+INSERT INTO animal 
+	(id, nombre, especie, sexo, color, fecha_nacimiento, nacimiento_en_cautiverio, id_padre, id_madre, id_recinto)
+	VALUES
+		(1, 'Copito de nieve', 'Orangután', 'M', 'Blanco', '2000-02-03', 0, null, null, 1),
+		(2, 'Rita', 'Orangután', 'H','Azul', '2001-02-03', 0, null, null, 1)
+GO
+
+INSERT INTO animal 
+	(id, nombre, especie, sexo, color, fecha_nacimiento, nacimiento_en_cautiverio, id_padre, id_madre, id_recinto)
+	VALUES
+		(3, 'Copito de nieve junior', 'Orangután', 'H','Negro', '2015-02-03', 1, 1, 2, 1)
+GO
+
+INSERT INTO espectaculo (id, nome, fecha, id_tarifa, id_recinto)
+	VALUES 
+		(1, 'Orangutanes volando', '2018-01-01', 1, 1),
+		(2, 'Orangutanes bailando', '2018-02-02', 3, 3),
+		(3, 'Orangutanes cantando', '2018-02-03', 2, 2)
+GO
+
+INSERT INTO recinto_mantenimiento(id_mantenimiento, id_recinto)
+	VALUES 
+		(1, 1),
+		(2, 2)
+GO
+
+INSERT INTO recinto_limpiador (id_limpiador, id_recinto)
+	VALUES 
+		(1, 1),
+		(2, 2)
+GO
+
+INSERT INTO animal_cuidador (id_animal, id_cuidador)
+	VALUES 
+		(1, 1),
+		(2, 2)
+GO
+
+INSERT INTO animal_veterinario(id_animal, id_veterinario)
+	VALUES 
+		(1, 1),
+		(2, 2)
+GO
+
+INSERT INTO animal_espectaculo_cuidador (id_animal, id_espectaculo, id_cuidador, horario)
+	VALUES 
+		(1, 1, 1, 'Todos los días de 19:00 a21:00'),
+		(2, 2, 2, 'Todos los sábados a las 16:00')
+
+GO
+
+
+
+
+
+
+
+			
